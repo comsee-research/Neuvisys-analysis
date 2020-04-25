128e6fea4210a85118b611d9dbf87b15430d1d18
Alphat18 <thomas.barbier@uca.fr>
2020-04-25 03:11:44 +0200

    more diverse visualization

--- configuration_planner.py
+++ configuration_planner.py
@@ -26,7 +26,7 @@ from scipy.stats import norm
 
 from fpdf import FPDF
 
-def generate_pdf(directory, title, rows, cols, nb_synapses):
+def generate_pdf(directory, title, rows, cols, nb_synapses, nb_layers, layer):
     header = 30
     images = natsorted(os.listdir(directory))
     pdf = FPDF("P", "mm", (cols*11, header+rows*11*nb_synapses))
@@ -35,12 +35,30 @@ def generate_pdf(directory, title, rows, cols, nb_synapses):
     pdf.set_font('Arial', '', 10)
     pdf.multi_cell(0, 5, title)
     
-    count  = 0    
+    count = nb_synapses * layer
     for i in range(cols):
         for j in range(rows):
             for s in range(nb_synapses):
                 pdf.image(directory+images[count], x=i*11, y=header+j*11*nb_synapses+s*10.4, w=10, h=10)
                 count += 1
+            count += nb_synapses * (nb_layers - 1)
+    return pdf
+
+def generate_pdf_layers(directory, title, rows, cols, nb_synapses, nb_layers):
+    header = 30
+    images = natsorted(os.listdir(directory))
+    pdf = FPDF("P", "mm", (cols*11, header+rows*11*nb_layers))
+    pdf.add_page()
+    
+    pdf.set_font('Arial', '', 10)
+    pdf.multi_cell(0, 5, title)
+    
+    count = 0
+    for i in range(cols):
+        for j in range(rows):
+            for l in range(nb_layers):
+                pdf.image(directory+images[count], x=i*11, y=header+j*11*nb_layers+l*10.4, w=10, h=10)
+                count += nb_synapses
     return pdf
 
 def compress_weight(weights, path):
@@ -73,15 +91,14 @@ def plot_network(directory, network_id):
         pass
     
     for i, file in enumerate(files):
-        if i % network_params["NETWORK_DEPTH"] == 0:
-            for synapse in range(network_params["NEURON_SYNAPSES"]):
-                if network_params["NEURON_SYNAPSES"] < 2:
-                    weights = np.moveaxis(np.concatenate((np.load(file), np.zeros((1, network_params["NEURON_WIDTH"], network_params["NEURON_HEIGHT"]))), axis=0), 0, 2)
-                else:
-                    weights = np.moveaxis(np.concatenate((np.load(file)[:, synapse], np.zeros((1, network_params["NEURON_WIDTH"], network_params["NEURON_HEIGHT"]))), axis=0), 0, 2)     
-                    
-                weights = np.kron(weights, np.ones((3, 3, 1)))
-                compress_weight(weights, directory+"images/"+str(network_id)+"/"+str(i)+"_syn"+str(synapse)+".png")
+        for synapse in range(network_params["NEURON_SYNAPSES"]):
+            if network_params["NEURON_SYNAPSES"] < 2:
+                weights = np.moveaxis(np.concatenate((np.load(file), np.zeros((1, network_params["NEURON_WIDTH"], network_params["NEURON_HEIGHT"]))), axis=0), 0, 2)
+            else:
+                weights = np.moveaxis(np.concatenate((np.load(file)[:, synapse], np.zeros((1, network_params["NEURON_WIDTH"], network_params["NEURON_HEIGHT"]))), axis=0), 0, 2)     
+                
+            weights = np.kron(weights, np.ones((3, 3, 1)))
+            compress_weight(weights, directory+"images/"+str(network_id)+"/"+str(i)+"_syn"+str(synapse)+".png")
     return network_params
 
     # sizes = [os.path.getsize(directory+"images/"+str(network_id)+"/"+file) for file in natsorted(os.listdir(directory+"images/"+str(network_id))) if file.find("syn"+str(synapse)) != -1]
@@ -102,6 +119,7 @@ def generate_multiple_configurations(directory, sampler, n_iter):
         except:
             print("Finished learning: " + str(i))
 
+
 param_grid = {"NEURON_WIDTH": [10], "NEURON_HEIGHT": [10], "NEURON_SYNAPSES": [2, 3], "SYNAPSE_DELAY": [5000, 10000, 15000, 20000, 25000], "X_ANCHOR_POINT": [0], "Y_ANCHOR_POINT": [0], "NETWORK_WIDTH": [34], "NETWORK_HEIGHT": [26], "NETWORK_DEPTH": [1], "DELTA_VP": [0.06], "DELTA_VD": [0.02], "DELTA_SR": [0.1], "TAU_LTP": [10000], "TAU_LTD": [20000], "VTHRESH": [20, 40, 60, 80], "VRESET": [-10], "TAU_M": [10000], "TAU_INHIB": [5000], "NORM_FACTOR": [4], "NORM_THRESHOLD": [4], "TARGET_SPIKE_RATE": [0.5]}
     
 directory = "/home/thomas/neuvisys-dv/configuration/"
@@ -110,7 +128,10 @@ n_iter = len(sampler)
 
 for network_id in range(1):
     network_params = plot_network(directory, network_id)
-    pdf = generate_pdf(directory+"/images/"+str(network_id)+"/", str(network_params), network_params["NETWORK_HEIGHT"], network_params["NETWORK_WIDTH"], network_params["NEURON_SYNAPSES"])
-    pdf.output(directory+"/figures/"+str(network_id)+".pdf", "F")
-
+    for layer in range(network_params["NETWORK_DEPTH"]):
+        pdf = generate_pdf(directory+"/images/"+str(network_id)+"/", str(network_params), network_params["NETWORK_HEIGHT"], network_params["NETWORK_WIDTH"], network_params["NEURON_SYNAPSES"], network_params["NETWORK_DEPTH"], layer)
+        pdf.output(directory+"/figures/"+str(network_id)+"_"+str(layer)+".pdf", "F")
+        
+    pdf = generate_pdf_layers(directory+"/images/"+str(network_id)+"/", str(network_params), network_params["NETWORK_HEIGHT"], network_params["NETWORK_WIDTH"], network_params["NEURON_SYNAPSES"], network_params["NETWORK_DEPTH"])
+    pdf.output(directory+"/figures/"+str(network_id)+"_multi_layer.pdf", "F")
 # generate_multiple_configurations(directory, sampler, n_iter)

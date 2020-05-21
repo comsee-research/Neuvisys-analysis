197a95d8fe071b30203a52da14276c0771cd5389
Alphat18 <thomas.barbier@uca.fr>
2020-05-21 14:27:00 +0200

    new planner and spike train statistics

--- configuration_planning/planner.py
+++ configuration_planning/planner.py
@@ -13,6 +13,27 @@ import os
 import numpy as np
 from natsort import natsorted
 
+def generate_multiple_configurations(directory, sampler, n_iter, learning_time):
+    for i in range(n_iter):
+        os.mkdir(directory+"network_"+str(i))
+        os.mkdir(directory+"network_"+str(i)+"/weights")
+        os.mkdir(directory+"network_"+str(i)+"/configs")
+        os.mkdir(directory+"network_"+str(i)+"/figures")
+        os.mkdir(directory+"network_"+str(i)+"/images")
+        
+        with open(directory+"network_"+str(i)+"/configs/config.json", "w") as file:
+            json.dump(sampler[i], file)
+
+        with open(directory+"conf.json", "w") as conf:
+            json.dump({"SAVE_DATA": True,
+                       "SAVE_DATA_LOCATION": directory+"network_"+str(i)+"/weights/",
+                       "CONF_FILES_LOCATION": directory+"network_"+str(i)+"/configs/config.json"}, conf)
+    
+        try:
+            subprocess.run(["dv-runtime", "-b0"], timeout=learning_time)
+        except:
+            print("Finished learning: " + str(i))
+
 # "DELTA_VP": norm(loc=0.065, scale=0.015),
 # "DELTA_VD": norm(loc=0.025, scale=0.015),
 # "TAU_LTP": norm(loc=10000, scale=5000),
@@ -22,27 +43,36 @@ from natsort import natsorted
 # "TAU_M": norm(loc=12000, scale=6000),
 # "TAU_INHIB": norm(loc=8500, scale=3500),
 
+param_grid = {
+	"NEURON_WIDTH": 10,
+	"NEURON_HEIGHT": 10,
+	"NEURON_SYNAPSES": 1,
+	"SYNAPSE_DELAY": 0,
+	"X_ANCHOR_POINT": 0,
+	"Y_ANCHOR_POINT": 0,
+	"NETWORK_WIDTH": 34,
+	"NETWORK_HEIGHT": 26,
+	"NETWORK_DEPTH": 4,
+	"DELTA_VP": 0.077,
+	"DELTA_VD": 0.021,
+	"DELTA_SR": 1,
+	"DELTA_RP": 1,
+	"DELTA_SRA": 0.06,
+	"DELTA_INH": 100,
+	"TAU_LTP": 7000,
+	"TAU_LTD": 14000,
+	"TAU_M": 18000,
+	"TAU_RP": 25000,
+	"TAU_SRA": 100000,
+	"VTHRESH": 30,
+	"VRESET": -20,
+	"DECAY_FACTOR": 0.995,
+	"NORM_FACTOR": 4,
+	"TARGET_SPIKE_RATE": 0.75
+}
 
-def generate_multiple_configurations(directory, sampler, n_iter):
-    for i in range(n_iter):
-        with open(directory+"configs/"+str(i)+".json", "w") as file:
-            json.dump(sampler[i], file)
-
-        os.mkdir(directory+"weights/"+str(i))
-        with open("/home/thomas/neuvisys-dv/configs/conf.json", "w") as conf:
-            json.dump({"SAVE_DATA": True,
-                       "SAVE_DATA_LOCATION": directory+"weights/"+str(i)+"/",
-                       "CONF_FILES_LOCATION": directory+"configs/"+str(i)+".json"}, conf)
-    
-        try:
-            subprocess.run(["dv-runtime", "-b0"], timeout=190)
-        except:
-            print("Finished learning: " + str(i))
-
-
-param_grid = {"NEURON_WIDTH": [10], "NEURON_HEIGHT": [10], "NEURON_SYNAPSES": [2, 3], "SYNAPSE_DELAY": [5000, 10000, 15000, 20000, 25000], "X_ANCHOR_POINT": [0], "Y_ANCHOR_POINT": [0], "NETWORK_WIDTH": [34], "NETWORK_HEIGHT": [26], "NETWORK_DEPTH": [1], "DELTA_VP": [0.06], "DELTA_VD": [0.02], "DELTA_SR": [0.1], "TAU_LTP": [10000], "TAU_LTD": [20000], "VTHRESH": [20, 40, 60, 80], "VRESET": [-10], "TAU_M": [10000], "TAU_INHIB": [5000], "NORM_FACTOR": [4], "NORM_THRESHOLD": [4], "TARGET_SPIKE_RATE": [0.5]}
-    
 directory = "/home/thomas/neuvisys-dv/configuration/"
-network_id = 1
 sampler = list(ParameterGrid(param_grid))
 n_iter = len(sampler)
+
+generate_multiple_configurations(directory, sampler, n_iter, 360)
--- statistical_properties/spike_train.py
+++ statistical_properties/spike_train.py
@@ -17,15 +17,36 @@ spike_trains = []
 for i in range(3346):
     spike_trains.append(np.array(load_params(directory+"weights/neuron_"+str(i)+".json")["spike_train"]))
 
-argmax = np.argmax([len(spike_trains[i]) for i in range(len(spike_trains))])
+n = 20
+argmaxs = np.argpartition([len(spike_trains[i]) for i in range(len(spike_trains))], -4)[-n:]
+nb_spikes = np.array([len(spike_trains[i]) for i in range(len(spike_trains))])
 
-spike_train = spike_trains[argmax]
-isi = (spike_train[1:] - spike_train[:-1]) / 1000
+print("average standard deviation for inhibited neurons")
+nb_spikes_inh = [nb_spikes[i:i+4] for i in range(0, nb_spikes.size, 4)]
+print(np.mean([np.std(a) for a in nb_spikes_inh]))
 
-# Plot spike train
 plt.figure()
-plt.eventplot(spike_train)
+plt.title("histogram of the neurons spiking rate")
+plt.xlabel("spiking rate (spikes/s)")
+plt.hist(nb_spikes / 360)
 
-# ISI histogram
-plt.figure()
-plt.hist(isi[isi < 2000], bins="auto")
+isis = []
+for arg in argmaxs:
+    spike_train = spike_trains[arg]
+    isi = (spike_train[1:] - spike_train[:-1]) / 1000
+    isis.append(isi)
+    
+    plt.figure()
+    plt.title("ISI histogram")
+    plt.xlabel("interspike interval (ms)")
+    plt.xticks(np.arange(0, 701, 50))
+    plt.xlim(left=0)
+    plt.hist(isi[isi < 700], bins=30)
+
+print("total number of spikes per layer")
+for i in range(4):
+    print(np.sum(nb_spikes[i::4]))
+
+# plt.figure()
+# plt.title("spike train")
+# plt.eventplot(spike_train)

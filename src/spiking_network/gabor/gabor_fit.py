ab1e6e0c7cf43c9a73bed54fef0a0af46b9f4909
Alphat18 <thomas.barbier@uca.fr>
2022-06-17 18:53:19 +0200

    gabor python code

--- src/spiking_network/gabor/gabor_fit.py
+++ src/spiking_network/gabor/gabor_fit.py
@@ -2,34 +2,39 @@ import numpy as np
 from scipy.optimize import curve_fit
 
 
-def gabor_fit_one_patch(basis):
+def gabor_fit_one_patch(basis: np.ndarray, rf_size: tuple, nb_cameras=1):
+    size_basis = basis.shape[0]
+    if nb_cameras == 2:
+        size_basis = size_basis // 2
     num_basis = basis.shape[1]
-    xi, yi = np.meshgrid(np.arange(10), np.arange(10))
-    true_basis = np.zeros((100, num_basis))
-    est_basis = np.zeros((100, num_basis))
+    xi, yi = np.meshgrid(np.arange(rf_size[0]), np.arange(rf_size[1]))
 
-    mu_table = np.zeros((2, num_basis))
-    lambda_table = np.zeros(num_basis)
-    sigma_table = np.zeros((2, num_basis))
-    theta_table = np.zeros(num_basis)
-    phase_table = np.zeros(num_basis)
-    error_table = np.zeros(num_basis)
+    true_basis = np.zeros((nb_cameras, size_basis, num_basis))
+    est_basis = np.zeros((nb_cameras, size_basis, num_basis))
+    mu_table = np.zeros((nb_cameras, 2, num_basis))
+    sigma_table = np.zeros((nb_cameras, 2, num_basis))
+    lambda_table = np.zeros((nb_cameras, num_basis))
+    theta_table = np.zeros((nb_cameras, num_basis))
+    phase_table = np.zeros((nb_cameras, num_basis))
+    error_table = np.zeros((nb_cameras, num_basis))
 
-    for i in range(num_basis):
-        print('Iteration:', str(i))
+    for cam in range(nb_cameras):
+        for i in range(num_basis):
+            print("Camera: ", str(cam), "Iteration: ", str(i))
 
-        b1 = basis[0:100, i]
-        z = np.reshape(b1[0:100], (10, 10))
-        x, g, sse = auto_gabor_surf_one_patch(xi, yi, z)
+            b1 = basis[cam * size_basis:(cam + 1) * size_basis, i]
+            z = np.reshape(b1, (rf_size[0], rf_size[1]))
+            x, g, sse = auto_gabor_surf_one_patch(xi, yi, z)
 
-        est_basis[:, i] = g.flatten()
-        true_basis[:, i] = b1
-        mu_table[:, i] = [x[0], x[1]]
-        lambda_table[i] = x[2]
-        sigma_table[:, i] = [x[3], x[4]]
-        theta_table[i] = x[5]
-        phase_table[i] = x[6]
-        error_table[i] = sse
+            est_basis[cam, :, i] = g.flatten()
+            true_basis[cam, :, i] = b1
+            mu_table[cam, :, i] = [x[0], x[1]]
+            lambda_table[cam, i] = x[2]
+            sigma_table[cam, :, i] = [x[3], x[4]]
+            theta_table[cam, i] = x[5]
+            phase_table[cam, i] = x[6]
+            error_table[cam, i] = sse
+        break
     return est_basis, true_basis, mu_table, lambda_table, sigma_table, theta_table, phase_table, error_table
 
 
@@ -40,23 +45,23 @@ def auto_gabor_surf_one_patch(xi, yi, z):
     sigmay = 5
     phase = 0
     ps = np.zeros(9)
-    error = 100
+    error = 200
 
-    for muy in range(0, 10, 5):
-        for mux in range(0, 10, 5):
+    for muy in range(0, 11, 5):
+        for mux in range(0, 11, 5):
             p0 = [mux, muy, lamb, sigmax, sigmay, theta, phase, 1, 0]
             long_zi = z.flatten()
             lb = [-np.inf, -np.inf, 2, 0, 0, 0, 0, 0, -np.inf]
             ub = [np.inf, np.inf, 20, 50, 50, np.pi, 2 * np.pi, np.inf, np.inf]
             try:
-                popt, pcov = curve_fit(eval_gabor_one_patch, [xi.flatten(), yi.flatten()], long_zi, p0, bounds=(lb, ub))
+                popt, pcov = curve_fit(eval_gabor_one_patch, [xi.flatten(), yi.flatten()], long_zi, p0, bounds=(lb, ub),
+                                       jac=jacobian, maxfev=5000)
                 perr = np.sqrt(np.diag(pcov))
                 if np.sum(perr) < error:
                     error = np.sum(perr)
                     ps = popt
-            except RuntimeError:
-                pass
-
+            except RuntimeError as e:
+                print(e)
 
     # H = np.array([[np.cos(ps[5]), -np.sin(ps[5])],
     #               [np.sin(ps[5]), np.cos(ps[5])]])
@@ -71,16 +76,22 @@ def eval_gabor_one_patch(xdata, mux, muy, lamb, sigmax, sigmay, theta, phase, a,
     xi = xdata[0, :]
     yi = xdata[1, :]
     z = eval_gabor_sep(ps, xi, yi)
-
     return z
-    # J = np.zeros((xdata.shape[0], len(ps)))
-    # for i in range(len(ps)):
-    #     delta = np.zeros(len(ps))
-    #     delta[i] = 1e-5
-    #     est_z = eval_gabor_sep(ps + delta, xi, yi)
-    #     J[:, i] = (est_z[:, i] - z[:, i]) / 1e-5
-    #
-    # return z, J
+
+
+def jacobian(xdata, mux, muy, lamb, sigmax, sigmay, theta, phase, a, b):
+    ps = [mux, muy, lamb, sigmax, sigmay, theta, phase, a, b]
+    xi = xdata[0, :]
+    yi = xdata[1, :]
+    z = eval_gabor_sep(ps, xi, yi)
+
+    jac = np.zeros((xdata.shape[1], len(ps)))
+    for i in range(len(ps)):
+        delta = np.zeros(len(ps))
+        delta[i] = 1e-5
+        est_z = eval_gabor_sep(ps + delta, xi, yi)
+        jac[:, i] = (est_z - z) / 1e-5
+    return jac
 
 
 def eval_gabor_sep(ps, xi, yi):
